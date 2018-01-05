2606b49b95c019f4f20fc6237ea0e71c6904469e
muriloventuroso <muriloventuroso@gmail.com>
2018-01-05 16:29:32 -0200

    Update

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -e
 
-echo 'Starting RabbitMQ'
-/etc/init.d/rabbitmq-server start
-echo 'Starting Redis'
-/etc/init.d/redis-server start
-echo 'Starting supervisor'
-/etc/init.d/supervisor start
-
 echo 'Cleaning lock files'
 rm -f /tmp/*.lock
 
